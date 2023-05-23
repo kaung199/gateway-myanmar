33ee3364a85d9163071f2562a0be9a54572ba95d
hakhant21 <hakhant21@gmail.com>
2023-05-23 14:10:00 +0630

    config file update

--- config/gateway.php
+++ config/gateway.php
@@ -1,14 +1,14 @@
 <?php
 
 return [
-    'merchant_id' => env('MERCHANT_ID'),
-    'secret_key' => env('SECRET_KEY'),
+    'merchant_id' => 'JT02',
+    'secret_key' => '72B8F060B3B923E580411200068A764610F61034AE729AB9EF20CAFF93AFA1B9',
     'currency_code' => 'MMK',
     'payment_channel' => ['CC', 'MPU', 'QR', 'DPAY', 'IMBANK'],
-    'frontend_result_url' => env('RESULT_URL_1'),
-    'backend_result_url' => env('RESULT_URL_2'),
+    'frontend_result_url' => 'http://project.test/checkout',
+    'backend_result_url' => 'http://project.test/checkout',
     'gateway_url' => [
-        'url' => env('BASE_URL'),
+        'url' => 'https://sandbox-pgw.2c2p.com/payment/4.1/',
         'token' => 'paymentToken',
         'inquiry' => 'paymentInquiry'
     ],
